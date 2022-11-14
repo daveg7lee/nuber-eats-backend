1634153e706dbe09c9e58f2f56f4036253dd36aa
daveg7lee <daveg7lee@gmail.com>
2022-11-14 22:49:23 +0900

    Our First Entity

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,10 +1,30 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { IsString, Length } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
-  @Field(() => String)
-  @IsString()
-  @Length(5, 10)
+  @PrimaryGeneratedColumn()
+  @Field((type) => Number)
+  id: number;
+
+  @Field((type) => String)
+  @Column()
   name: string;
+
+  @Field((type) => Boolean)
+  @Column()
+  isVegan: boolean;
+
+  @Field((type) => String)
+  @Column()
+  address: string;
+
+  @Field((type) => String)
+  @Column()
+  ownersName: string;
+
+  @Field((type) => String)
+  @Column()
+  categoryName: string;
 }
