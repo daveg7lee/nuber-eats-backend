9864e8ede19122186edaf680ff28dfbed9fb1cfc
daveg7lee <daveg7lee@gmail.com>
2023-01-08 15:14:39 +0900

    Testing createAccount part One

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,7 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
-import { getConnection } from 'typeorm';
+import { DataSource, getConnection } from 'typeorm';
+import * as request from 'supertest';
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -16,13 +19,50 @@ describe('UserModule (e2e)', () => {
   });
 
   afterAll(async () => {
-    await getConnection().dropDatabase();
+    const dataSource = new DataSource({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'dave',
+      password: '12345',
+      database: 'nuber-eats-test',
+    });
+    const connection = await dataSource.initialize();
+    await connection.dropDatabase();
+    await connection.destroy();
+
     app.close();
+  });
+
+  describe('createAccount', () => {
+    const EMAIL = 'daveg7lee@gmail.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email: "${EMAIL}",
+              password: "12345",
+              role: Owner
+            }) {
+              ok
+              error
+            }
+          }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBeTruthy();
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
   });
 
   it.todo('me');
   it.todo('userProfile');
-  it.todo('createAccount');
   it.todo('login');
   it.todo('editProfile');
   it.todo('verifyEmail');
