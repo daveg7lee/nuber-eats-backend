d32972a6edcf10a16406ad2b6851798d96442603
daveg7lee <daveg7lee@gmail.com>
2024-02-20 21:04:03 +0900

    Create Order part Two

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -4,19 +4,35 @@ import { Order } from './entities/order.entity';
 import { Repository } from 'typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 
 @Injectable()
 export class OrderService {
   constructor(
     @InjectRepository(Order) private readonly orders: Repository<Order>,
+    @InjectRepository(Restaurant)
+    private readonly restaurants: Repository<Restaurant>,
   ) {}
 
   async createOrder(
     customer: User,
-    createOrderInput: CreateOrderInput,
+    { restaurantId, items }: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
-    return {
-      ok: false,
-    };
+    try {
+      const restaurant = await this.restaurants.findOne({
+        where: { id: restaurantId },
+      });
+      if (!restaurant) {
+        return {
+          ok: false,
+          error: 'Restaurant not found',
+        };
+      }
+    } catch (e) {
+      return {
+        ok: false,
+        error: 'Could not create order',
+      };
+    }
   }
 }
