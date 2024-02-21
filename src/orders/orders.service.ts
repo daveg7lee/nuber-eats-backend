3a3bb26c6d6b12633be0a2b4fbc0609a5493d6bd
daveg7lee <daveg7lee@gmail.com>
2024-02-21 19:03:43 +0900

    Create Order part Five

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -33,15 +33,35 @@ export class OrderService {
           error: 'Restaurant not found',
         };
       }
-      items.forEach(async (item) => {
+      for (const item of items) {
         const dish = await this.dishes.findOne({ where: { id: item.dishId } });
         if (!dish) {
-          // abort this whole thing
+          return {
+            ok: false,
+            error: 'Dish not found.',
+          };
+        }
+        for (const itemOption of item.options) {
+          const dishOption = dish.options.find(
+            (dishOption) => dishOption.name === itemOption.name,
+          );
+          if (dishOption) {
+            if (dishOption.extra) {
+            } else {
+              const dishOptionChoice = dishOption.choices.find(
+                (optionChoice) => optionChoice.name === itemOption.choice,
+              );
+              if (dishOptionChoice) {
+                if (dishOptionChoice.extra) {
+                }
+              }
+            }
+          }
         }
         await this.orderItems.save(
           this.orderItems.create({ dish, options: item.options }),
         );
-      });
+      }
       const order = await this.orders.save(
         this.orders.create({
           customer,
